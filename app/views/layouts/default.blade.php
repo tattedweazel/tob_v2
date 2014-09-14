fd7c6b5bd2d6f486b739638e4ad31cc0d12f187f
Dave Pratt <dave@paracore.com>
2014-09-13 23:14:27 -0700

    Forgot some meta to make it betta

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $section_title }} :: {{ $site_name }}</title>
 
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
